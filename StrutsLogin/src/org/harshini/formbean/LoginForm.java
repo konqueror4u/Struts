e0028594102ebceffed47fa694069c800c1e2f74
viswanath <viswanath@king>
2012-12-22 18:55:49 +0530

    updated code

---
+++ StrutsLogin/src/org/harshini/formbean/LoginForm.java
@@ -0,0 +1,27 @@
+package org.harshini.formbean;
+
+import org.apache.struts.action.ActionForm;
+
+public class LoginForm extends ActionForm {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String userName;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	private String password;
+	
+}
