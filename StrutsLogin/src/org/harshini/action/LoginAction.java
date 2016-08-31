f89c499b073fafdbec94ddd2796aebb0c197ffcf
visu <visusomanchi@gmail.com>
2016-08-30 21:29:54 -0700

    updated in othe brach

--- StrutsLogin/src/org/harshini/action/LoginAction.java
+++ StrutsLogin/src/org/harshini/action/LoginAction.java
@@ -14,7 +14,7 @@ public class LoginAction  extends Action{
 			HttpServletRequest request, HttpServletResponse response) {
 		
 		LoginForm loginForm = (LoginForm) form;
-		System.out.println("test1");
+		System.out.println("test2");
 		return mapping.findForward("success");
 		
 	}
